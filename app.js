fc71f310edf05b614c05c73dcc46b06d224f04a5
NischalShrestha <nischalshrestha324@gmain.com>
2023-08-04 17:00:59 +0545

    challange 7 completed

--- app.js
+++ app.js
@@ -33,6 +33,10 @@ app.get("/contact", (req, res) => {
   });
 });
 
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
